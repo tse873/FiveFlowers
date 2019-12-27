660f28ae2c771685df5685ccb22ed9743638040c
tse873 <ashuang873@163.com>
2019-12-27 21:00:43 +0800

    whjx

--- src/main/java/com/whjx/service/impl/HomeShopDisplayServiceImpl.java
+++ src/main/java/com/whjx/service/impl/HomeShopDisplayServiceImpl.java
@@ -4,6 +4,8 @@ import com.whjx.dao.HomeShopDisplayMapper;
 import com.whjx.pojo.Spu;
 import com.whjx.service.HomeShopDisplayService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,6 +14,8 @@ import java.util.List;
 public class HomeShopDisplayServiceImpl implements HomeShopDisplayService {
     @Autowired
     private HomeShopDisplayMapper homeShopDisplayMapper;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Override
     public List<Spu> selectAllByPrimaryCategories(int pcid) {
