5072fc79d87c315104773f0d12b472ae00a8c8bb
ypeiyuan <ypeiyuan@163.com>
2020-01-07 11:16:55 +0800

    ypy

--- src/main/java/com/whjx/service/OrderService.java
+++ src/main/java/com/whjx/service/OrderService.java
@@ -7,4 +7,5 @@ import java.util.List;
 public interface OrderService {
     List<Order> getOrderAll(Order order);
     Order getOrderByStatus(int orderDeliveryStatus);
+    String updateOrderByStatus(Order order);
 }
